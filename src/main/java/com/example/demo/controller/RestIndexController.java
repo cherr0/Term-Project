2e94ab8677d4337d6269ef3aa04dd5d3eee061db
Cherr0 <adsl1664@gmail.com>
2020-12-05 19:17:19 +0900

    회원가입 메일 인증 기능 추가

--- src/main/java/com/example/demo/controller/RestIndexController.java
+++ src/main/java/com/example/demo/controller/RestIndexController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Email;
 import com.example.demo.domain.Member;
+import com.example.demo.service.EmailService;
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +14,11 @@ import javax.servlet.http.HttpSession;
 public class RestIndexController {
     @Autowired
     MemberService memberService;
+    @Autowired
+    EmailService emailService;
 
+
+    // 회원가입
     @PostMapping("/rest/join")
     public void join(Member member){
         System.out.println("restController join(): " + member.toString());
@@ -20,12 +26,34 @@ public class RestIndexController {
         memberService.join(member);
     }
 
+    // 로그인
     @PostMapping("rest/login")
     public Member login(Member member, HttpSession httpSession){
-        System.out.println("restcontroller login(): " + member.toString());
+        System.out.println("restController login(): " + member.toString());
         System.out.println("restController login() return : " + memberService.login(member));
         httpSession.setAttribute("user", memberService.login(member));  // 세션에 유저 등록
         return memberService.login(member);
     }
+
+    // 메일 전송
+    @PostMapping("rest/email")
+    public void sendMail(Email email) throws Exception {
+        emailService.sendSimpleMessage(email.getUserEmail());
+        System.out.println("메일 전송 완료");
+    }
+
+    // 인증 코드
+    @PostMapping("rest/confirm")
+    public int confirm(Email email) {
+        if(emailService.ePw.equals(email.getConfirm())){
+            System.out.println("적은 인증번호() : " + email.getConfirm());
+            return EmailService.CONFIRM;
+        }else {
+            System.out.println("적은 인증번호() : " + email.getConfirm());
+            return EmailService.REJECT;
+        }
+    }
+
+
 
 }
---
+++ src/main/java/com/example/demo/domain/Email.java
@@ -0,0 +1,9 @@
+package com.example.demo.domain;
+
+import lombok.Data;
+
+@Data
+public class Email {
+    private String userEmail;
+    private String confirm;
+}
