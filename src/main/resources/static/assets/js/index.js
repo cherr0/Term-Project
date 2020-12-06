b857f121f6eafa7ad69c31d4bc340ec514d778ff
cherro <adsl1664@gmail.com>
2020-12-06 22:43:05 +0900

    프론트 JS 추가

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
     $('#join').on('click', function () {
         let data = {
             id : $('#joinId').val(),
-            password : $('#joinPW').val(),
-            name : $('#name').val(),
+            password : $('#joinPw').val(),
+            name : $('#joinName').val(),
             email : $('#joinEmail').val(),
             rePw : $('#repeatJoinPw').val()
         }
 
-        if(data.rePw === data.password){
+        if(data.rePw !== data.password){
             alert('비밀번호 확인 값이 일치하지 않습니다.');
             return;
         }
@@ -50,8 +50,8 @@ $(document).ready(function() {
             data : data,
             type : 'post',
             url : '/rest/login',
-            success : function (verify) {
-                if(verify === 1){
+            success : function (member) {
+                if(member === 1){
                     alert('로그인 성공');
                     window.location.href = '/index';
                 }else {
@@ -102,12 +102,151 @@ $(document).ready(function() {
             }
         });
     });
+
+    // 뒤로가기
+    $('#back').on('click', function () {
+        window.location.href = '/index';
+    });
+
+    // 글쓰기
+    $('#write').on('click', function() {
+        let data = {
+            title : $('#title').val(),
+            content : $('#content').val(),
+            writer : $('#writer').val(),
+            mno : $('#mno').val()
+        }
+
+        $.ajax({
+            data : data,
+            type : 'post',
+            url : 'rest/write',
+            success : function() {
+                alert('글 등록 완료');
+                window.location.href = '/index' ;
+            }
+        });
+    });
+
+    // 추천
+    $('#reco').on('click', function() {
+        let data = {
+            bno : $('#bno').val()
+        }
+
+        $.ajax({
+            data : data,
+            type : 'put',
+            url : '/rest/reco',
+            success : function () {
+                alert('추천 완료');
+                location.reload();
+            }
+
+        });
+    });
+
+    // 글 삭제
+    $('#delete').on('click', function() {
+        let data = {
+            bno : $('#bno').val()
+        }
+
+        $.ajax({
+            data : data,
+            type : 'delete',
+            url : '/rest/delete',
+            success : function() {
+                alert('삭제 완료');
+                window.location.href = '/index';
+            }
+        });
+    });
+
+    // 수정
+    $('#update').on('click', function() {
+        let data = {
+            title : $('#title').val(),
+            content : $('#content').val(),
+            writer : $('#writer').val(),
+            bno : $('#bno').val()
+        }
+
+        $.ajax({
+            data : data,
+            type : 'put',
+            url : '/rest/update',
+            success : function() {
+                alert('수정 완료!');
+                window.location.href = '/select/' + data.bno;
+            }
+        });
+    });
+
+
+    // 글 수정 버튼
+    $('#updateBtn').on('click', function() {
+        let bno = $('#bno').val();
+        window.location.href = '/update/' + bno;
+    });
+
+    // 댓글
+    $('#reply').on('click', function() {
+        console.log('클릭확인');
+        let data = {
+            rcontent : $('#rcontent').val(),
+            rwriter : $('#rwriter').val(),
+            mno : $('#mno').val(),
+            bno : $('#bno').val()
+        }
+
+        console.log(data.rcontent);
+        console.log(data.rwriter);
+        console.log(data.mno);
+        console.log(data.bno);
+
+        $.ajax({
+            data : data,
+            type : 'post',
+            url : '/rest/reply',
+            success : function() {
+                alert('댓글 완료');
+                window.location.href = '/select/' + data.bno;
+            }
+        });
+    });
+
+
 });
 
-function checkJoin() {
+function up(temp) {
     let data = {
-        id : $('#joinId').val(),
-        pw : $('#joinPW').val(),
-        name : $('#name').val()
+        bno : $('#bno').val(),
+        rno : temp
     }
+
+    $.ajax({
+        data : data,
+        type : 'put',
+        url : '/rest/up',
+        success : function() {
+            location.reload();
+        }
+    });
+}
+
+function down(temp) {
+    let data = {
+        bno : $('#bno').val(),
+        rno : temp
+    }
+
+    $.ajax({
+        data : data,
+        type : 'put',
+        url : '/rest/down',
+        success : function() {
+            location.reload();
+        }
+    });
 }
