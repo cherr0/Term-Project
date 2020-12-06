af4b8c9f20658ab1573e058a22145f32c72f47ae
cherro <adsl1664@gmail.com>
2020-12-06 22:44:05 +0900

    Controller 수정

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Criteria;
 import com.example.demo.domain.Member;
+import com.example.demo.domain.Pagination;
+import com.example.demo.service.BoardService;
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
@@ -14,15 +19,30 @@ import java.util.ArrayList;
 public class IndexController {
     @Autowired
     MemberService memberService;
+    @Autowired
+    BoardService boardService;
 
 
+    // 기본 로그인 화면
     @GetMapping("/")
     public String loginForm(){
         return "loginForm";
     }
 
+    // 회원 가입
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    // 메인 화면
     @GetMapping("/index")
-    public String index(){
+    public String index(Model model, Criteria criteria, @RequestParam(defaultValue = "1") int page){
+        System.out.println("index IndexController");
+        Pagination pagination = new Pagination(boardService.boardListCnt(), page, 10);
+        criteria.setPage(pagination.getPage());
+        model.addAttribute("pagination",pagination);
+        model.addAttribute("boardList",boardService.boardList(criteria));
         return "index";
     }
 
@@ -33,10 +53,39 @@ public class IndexController {
         return "redirect:/";
     }
 
+    // 글쓰기
+    @GetMapping("/write")
+    public String write(){
+        return "write";
+    }
+
+
     // 회원 리스트 확인
     @GetMapping("/memberList")
-    public ArrayList<Member> memberList(Model model){
+    public String memberList(Model model){
         model.addAttribute("memberList", memberService.memberList());
-        return memberService.memberList();
+        return "memberList";
+    }
+
+
+    @GetMapping("/select/{bno}")
+    public String select(Model model, @PathVariable int bno,
+                         Criteria criteria, @RequestParam(defaultValue = "1") int page) {
+        Pagination pagination = new Pagination(boardService.replyListCnt(bno), page, 10);
+        criteria.setPage(pagination.getPage());
+        criteria.setBno(bno);
+        System.out.println("select bno : " + bno);
+        boardService.count(bno);
+        System.out.println("select() return :" + boardService.select(bno));
+        model.addAttribute("pagination", pagination);
+        model.addAttribute("select",boardService.select(bno));
+        model.addAttribute("reply",boardService.replyList(criteria));
+        return "select";
+    }
+
+    @GetMapping("/update/{bno}")
+    public String update(Model model, @PathVariable int bno){
+        model.addAttribute("select", boardService.select(bno));
+        return "update";
     }
 }
--- src/main/java/com/example/demo/controller/RestIndexController.java
+++ src/main/java/com/example/demo/controller/RestIndexController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Board;
 import com.example.demo.domain.Email;
 import com.example.demo.domain.Member;
+import com.example.demo.domain.Reply;
+import com.example.demo.service.BoardService;
 import com.example.demo.service.EmailService;
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
@@ -15,6 +19,8 @@ public class RestIndexController {
     MemberService memberService;
     @Autowired
     EmailService emailService;
+    @Autowired
+    BoardService boardService;
 
 
     // 회원가입
@@ -31,26 +37,14 @@ public class RestIndexController {
     public int loginCheck(Member member, HttpSession httpSession) {
         System.out.println("loginCheck()");
         var verify = 0;
-        if(memberService.login(member)){
-            httpSession.setAttribute("user", member);
+        if(memberService.loginChk(member)){
+            httpSession.setAttribute("user", memberService.login(member));
             verify = 1;
         }
         System.out.println(verify);
         return verify;
 
 
-    }
-
-    //로그아웃 처리
-    @RequestMapping("logout")
-    public ModelAndView logout(HttpSession session) {
-
-        memberService.logout(session);
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("login");
-        mav.addObject("msg", "logout");
-
-        return mav;
     }
 
     // 메일 전송
@@ -72,6 +66,48 @@ public class RestIndexController {
         }
     }
 
+    // 글작성
+    @PostMapping("/rest/write")
+    public void write(Board board){
+        System.out.println("restController write(): " + board.toString());
+        boardService.write(board);
+    }
+
+    @PutMapping("/rest/reco")
+    public void reco(int bno){
+        System.out.println("restController reco(): " + bno);
+        boardService.reco(bno);
+    }
+
+    // 글 삭제
+    @DeleteMapping("/rest/delete")
+    public void delete(int bno){
+        System.out.println("restContorller delete(): " + bno);
+        boardService.delete(bno);
+    }
+
+    // 글 수정
+    @PutMapping("/rest/update")
+    public void update(Board board){
+        boardService.update(board);
+    }
+
+    @PostMapping("/rest/reply")
+    public void reply(Reply reply){
+        boardService.reply(reply);
+    }
+
+    // 댓글 추천
+    @PutMapping("/rest/up")
+    public void up(Reply reply){
+        boardService.up(reply);
+    }
+
+    // 댓글 비추
+    @PutMapping("/rest/down")
+    public void down(Reply reply){
+        boardService.down(reply);
+    }
 
 
 }
